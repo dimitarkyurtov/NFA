6c3f744135c407ed35fd8972219ed2a95fd8ca93
dimitarkyurtov <dimitar_kiurtov@abv.bg>
2019-05-14 19:51:48 +0300

    Update main.cpp

--- NFA/main.cpp
+++ NFA/main.cpp
@@ -3,22 +3,14 @@
 #include"Automaton.h"
 #include<cstring>
 
+
+
 using namespace std;
 
 int main()
 {
     Transition  tr4('W', 'a', 'Q'), tr1('Q', 'a', 'R'), tr2('R', 'b', 'S'), tr3('S', 'a', 'T');
-    Automaton automat, automat2;
-    automat.addTransition(tr1);
-    automat.addTransition(tr4);
-    automat.addStartingState('W');
-    automat.addEndingState('R');
-
-    automat2.addTransition(tr2);
-    automat2.addTransition(tr3);
-    automat2.addStartingState('R');
-    automat2.addEndingState('T');
-    Automaton automat3("aaaa");
+    Automaton automat3("dcba+ad");
     automat3.print();
     return 0;
 }
