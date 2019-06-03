723b322b98fd4a117e1989b45c81944254ba2ad0
dimitarkyurtov <dimitar_kiurtov@abv.bg>
2019-06-03 23:28:21 +0300

    Update main.cpp

--- NFA/main.cpp
+++ NFA/main.cpp
@@ -2,15 +2,23 @@
 #include"Transition.h"
 #include"Automaton.h"
 #include<cstring>
-
-
+void * __gxx_personality_v0=0;
+void * _Unwind_Resume =0;
 
 using namespace std;
 
 int main()
 {
-    Transition  tr4('W', 'a', 'Q'), tr1('Q', 'a', 'R'), tr2('R', 'b', 'S'), tr3('S', 'a', 'T');
-    Automaton automat3("dcba+ad");
-    automat3.print();
+    Transition  tr4("q0", 'a', "q0"), tr1("q0", 'b', "q0"), tr2("q0", 'a', "q1"), tr3("q1", 'b', "q2");
+    Automaton automat;
+    automat.addTransition(tr4);
+    automat.addTransition(tr1);
+    automat.addTransition(tr2);
+    automat.addTransition(tr3);
+    automat.addStartingState("q0");
+    automat.addEndingState("q2");
+    automat.print();
+    cout << endl << endl;
+    cout << automat.isWordRecognisable("ababab");
     return 0;
 }
